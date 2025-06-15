2a40735a29ad5518ce08093e1f5efa0b9c629b50
ietzel <67884277+ietzel@users.noreply.github.com>
2025-06-15 18:18:27 -0400

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,1 +1,1 @@
-document.getElementById(description).textContent += "Concerns include hobbies, clothing, etc."
+document.getElementById(description).textContent += "Concerns include hobbies, clothing, etc. Rate each on a scale of 1-10 to make a pie chart."
